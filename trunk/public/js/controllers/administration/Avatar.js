e683b4b56f18e65c6edbcb48be1177e43864489b
ramses@cuatromedios.com <ramses@cuatromedios.com@557bcda3-fa61-2ba3-eae1-30e81f189bc8>
2014-10-29 19:19:47 +0000

    Avatar editándose

--- trunk/public/js/controllers/administration/Avatar.js
+++ trunk/public/js/controllers/administration/Avatar.js
@@ -1,7 +1,27 @@
 app.controller('ControllerAvatar', ['$scope', '$http', '$location', '$route', '$cookieStore',
     function ($scope, $http, $location, $route, $cookieStore) {
         $scope.avatar = {};
-        $scope.avatar.gender = 0;
+        $scope.avatar.body = 0;
         $scope.avatar.face = 0;
         $scope.avatar.hair = 0;
+
+        bodies =
+
+        console.log("ControllerAvatar");
+
+        $scope.changePart = function (what, where)
+        {
+            console.log(what, where);
+            $scope.avatar[what] += where;
+            if ($scope.avatar[what] > 3) $scope.avatar[what] = 0;
+            if ($scope.avatar[what] < 0) $scope.avatar[what] = 3;
+            $scope.updateID();
+        }
+
+        $scope.updateID = function ()
+        {
+            $scope.avatar.id = "a"+$scope.avatar.body+$scope.avatar.face+$scope.avatar.hair;
+        }
+
+        $scope.updateID();
     }]);
