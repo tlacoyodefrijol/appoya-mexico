05bc16e544d447fb70328d9a5401ce5e85585512
hector@cuatromedios.com <hector@cuatromedios.com@557bcda3-fa61-2ba3-eae1-30e81f189bc8>
2014-11-04 03:06:21 +0000

    Agregada animación en home

--- trunk/public/js/controllers/administration/Event.js
+++ trunk/public/js/controllers/administration/Event.js
@@ -47,7 +47,6 @@ app.controller('ControllerEvent', ['$scope', '$routeParams', 'FactoryEvents', 'F
                 $scope.event = data.data;
                 $scope.roleList = $scope.event.roles;
                 $scope.prizeSelected = $scope.event.prizes;
-                console.log($scope.event.kind)
                 if(typeof $scope.event.kind[0] === 'undefined' || $scope.event.kind[0]=== undefined || $scope.event.kind[0]=== ''){
                     $scope.event.kind = $scope.eventListType[0];
                     $scope.animId = 0;
